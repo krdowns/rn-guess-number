927ac317b9f2c22d64ddb44f722b0f3baf41f3c4
Kenny Downs <kdowns@repairpal.com>
2020-02-23 13:29:13 -0800

    added props to input for better ux

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title='Guess a Number' />
+      <Header title="Guess a Number" />
       <StartGameScreen />
     </View>
   );
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,54 +1,91 @@
-import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
 
-import Card from '../components/Card';
-import Colors from '../constants/colors';
+import Card from "../components/Card";
+import Colors from "../constants/colors";
+import Input from "../components/Input";
 
 const StartGameScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <TextInput />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title='Reset' color={Colors.accent} onPress={() => {}} />
-                    </View>
-                    <View style={styles.button}>
-                        <Button title='Confirm' color={Colors.primary} onPress={() => {}} />
-                    </View>
-                </View>
-            </Card>
-        </View>
-    );
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const numberInputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+            TouchableWithoutFeedback
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title="Reset" color={Colors.accent} onPress={() => {}} />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                color={Colors.primary}
+                onPress={() => {}}
+              />
+            </View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 10,
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 20,
-        marginVertical: 10
-    },
-    inputContainer: {
-        width: 300,
-        maxWidth: '80%',
-        alignItems: 'center'
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        paddingHorizontal: 15
-    },
-    button: {
-        width: 100
-    }
-
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center"
+  },
+  input: {
+    width: 50,
+    textAlign: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15
+  },
+  button: {
+    width: 100
+  }
 });
 
 export default StartGameScreen;
