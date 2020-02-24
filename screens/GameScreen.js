10ab31c44247901783bd46de99a64b42eb225393
Kenny Downs <kdowns@repairpal.com>
2020-02-23 17:31:32 -0800

    finished game logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, Alert, Button } from "react-native";
 
 import NumberContainer from "../components/NumberContainer";
@@ -19,10 +19,19 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
-
+  const [rounds, setRounds] = useState(0);
   //Ref allows to save without rerendering//
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
+
+  //Effect allows for side effects//
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(rounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
 
   const nextGuessHandler = direction => {
     if (
@@ -46,6 +55,7 @@ const GameScreen = props => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
+    setRounds(curRounds => curRounds + 1);
   };
 
   return (
