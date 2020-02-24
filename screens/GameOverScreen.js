bd08a7b491004e278c0b5e8d000a9570ddf9d8ab
Kenny Downs <kdowns@repairpal.com>
2020-02-23 17:47:49 -0800

    added styling to endgame screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+import Card from "../components/Card";
+
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Is Over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
-      <Button title="NEW GAME" onPress={props.onRestart} />
+      <Card style={styles.gameOverContainer}>
+        <View style={styles.textContainer}>
+          <Text>The Game Is Over!</Text>
+          <Text>Number of rounds: {props.roundsNumber}</Text>
+          <Text>Number was: {props.userNumber}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="NEW GAME" onPress={props.onRestart} />
+        </View>
+      </Card>
     </View>
   );
 };
@@ -17,6 +25,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gameOverContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    padding: 25
+  },
+  textContainer: {
+    alignItems: "center",
+    marginVertical: 10,
+    paddingVertical: 10
   }
 });
 
